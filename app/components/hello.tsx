222788b0f7e0e067af4ed823130a455860191f23
pedro <pedro.collares05@gmai.com>
2025-10-06 23:12:01 -0300

    first commit

---
+++ app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Hello(){
+    console.log("i am a client side component")
+
+    return(
+        <div>
+            <h1>hellllllooooooo I am a client side component </h1>
+        </div>
+    );
+}
